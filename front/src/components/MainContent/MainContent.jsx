08374bf3ac7c35d1fbe93e594eae09d4c3c8bdbd
Nargiz <mam.zigran@gmail.com>
2025-02-16 23:06:03 +0600

    SEO оптимизация

--- front/src/components/MainContent/MainContent.jsx
+++ front/src/components/MainContent/MainContent.jsx
@@ -23,7 +23,20 @@ const MainContent = () => {
         return (
             <main className={styles.main}>
                 <div className={styles.contentContainer}>
-                    {/* Мобильная версия */}
+                    <div className={styles.shapeLeft}>
+                        <div className={styles.box}>
+                            <p className={styles.intro}>
+                                Дипломированный психолог <br/>
+                                Гештальт-терапевт <br/>
+                                Интегрированный подход <br/>
+                                Ассоциированный член <br/>
+                                <span>ПСАГС/ПКГА</span>
+                            </p>
+                        </div>
+                        <NavLink to="/booking" className={styles.button}>
+                            Записаться
+                        </NavLink>
+                    </div>
                     <div className={styles.shapeLeft}>
                         <img
                             src="/images/mainST.jpg"
@@ -33,36 +46,25 @@ const MainContent = () => {
                         <div>
                             <p className={styles.mainText}>
                                 Нет ничего, что может произойти с человеком, чего я,
-                                будучи тоже человеком, не смогу с ним разделить; нет такого страха, который я не смогу понять;
-                                нет страдания, к которому я могу остаться бесчувственным – это заложено в моей человеческой
+                                будучи тоже человеком, не смогу с ним разделить; нет такого страха, который я не смогу
+                                понять;
+                                нет страдания, к которому я могу остаться бесчувственным – это заложено в моей
+                                человеческой
                                 природе.
-                                <br />
+                                <br/>
                                 Как бы ни была глубока травма этого человека, этого передо мной не надо стыдиться.
-                                <br />
+                                <br/>
                                 Я тоже беззащитен перед лицом травмы.
-                                <br />
+                                <br/>
                                 Что бы ни пережил человек, ему не обязательно оставаться с этим наедине.
-                                <br />
+                                <br/>
                                 Если выразить и разделить свои чувства, то с этого начинается исцеление.
-                                <br />
+                                <br/>
                             </p>
                             <p className={styles.quoteAuthor}>[Речел Наоми Ремен]</p>
                         </div>
                     </div>
-                    <div className={styles.shapeLeft}>
-                        <div className={styles.box}>
-                            <p className={styles.intro}>
-                                Дипломированный психолог <br />
-                                Гештальт-терапевт <br />
-                                Интегрированный подход <br />
-                                Ассоциированный член <br />
-                                <span>ПСАГС/ПКГА</span>
-                            </p>
-                        </div>
-                        <NavLink to="/booking" className={styles.button}>
-                            Записаться
-                        </NavLink>
-                    </div>
+
                 </div>
             </main>
         );
@@ -81,7 +83,7 @@ const MainContent = () => {
                     <div className={styles.shapeLeft}>
                         <div className={styles.box}>
                             <p className={styles.intro}>
-                                Дипломированный психолог <br />
+                            Дипломированный психолог <br />
                                 Гештальт-терапевт <br />
                                 Интегрированный подход <br />
                                 Ассоциированный член <br />
